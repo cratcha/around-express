b7982126da6e42309e458ed82788765dd8d7c659
Ekaterina Cratcha <ekaterina.cratcha@yandex.com>
2022-04-06 21:52:28 -0400

    finish set up

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const express = require("express");
+const express = require('express');
 // listen to port 3000
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
   // if everything works fine, the console will show which port the application is listening to
