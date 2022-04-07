2b6f64315d7862bc9fa558c7550d46958b8f9080
Ekaterina Cratcha <ekaterina.cratcha@yandex.com>
2022-04-06 23:32:45 -0400

    fix linter errors

--- controllers/cards.js
+++ controllers/cards.js
@@ -6,9 +6,9 @@ const cardsDataPath = path.join(__dirname, '../data/cards.json');
 const getCards = (req, res) => {
   getDataFromFile(cardsDataPath)
     .then((cards) => res.status(200).send(cards))
-    .catch(() =>
-      res.status(500).send({ message: 'An error has occured on the server' })
-    );
+    .catch(() => {
+      res.status(500).send({ message: 'An error has occured on the server' });
+    });
 };
 
 module.exports = { getCards };
